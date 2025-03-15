3443f4657173374912402931ecb48c4cf5c7d7b6
Idba1 <moniraislamself@gmail.com>
2025-03-15 07:12:55 +0600

    show data with section title

--- src/Components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/Components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -4,9 +4,8 @@ import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { Button } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { mens_kurta } from '../../../public/men_kurta'
 
-const HomeSectionCarosel = () => {
+const HomeSectionCarosel = ({ data, sectionName }) => {
     const carouselRef = useRef(null);
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -16,7 +15,7 @@ const HomeSectionCarosel = () => {
         1024: { items: 5 },
     };
 
-    const items = mens_kurta.slice(0, 15).map((item, index) => (
+    const items = data.slice(0, 15).map((item, index) => (
         <HomeSectionCard product={item} key={index} />
     ));
 
@@ -43,6 +42,7 @@ const HomeSectionCarosel = () => {
 
     return (
         <div className="px-4 lg:px-8">
+            <h2 className='text-2xl font-extrabold text-gray-800 py-5 lg:py-10'>{sectionName}</h2>
             <div className="relative p-5">
                 <AliceCarousel
                     ref={carouselRef}
--- src/Customer/Components/Pages/Home/Home.jsx
+++ src/Customer/Components/Pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import MainCarousel from '../../HomeCarosel/MainCarosel'
 import HomeSectionCarosel from '../../../../Components/HomeSectionCarosel/HomeSectionCarosel'
+import { mens_kurta } from '../../../../data/men_kurta'
+import { dressPage1 } from '../../../../data/dressPage1'
 
 function Home() {
     return (
         <div className='container mx-auto'>
             <MainCarousel></MainCarousel>
             <div className='space-y-10 py-20 flex flex-col justify-center'>
-                <HomeSectionCarosel></HomeSectionCarosel>
-                <HomeSectionCarosel></HomeSectionCarosel>
-                <HomeSectionCarosel></HomeSectionCarosel>
+                <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Kurta"}></HomeSectionCarosel>
+                <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shoes"}></HomeSectionCarosel>
+                <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shirt"}></HomeSectionCarosel>
+                <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Saree"}></HomeSectionCarosel>
+                <HomeSectionCarosel data={mens_kurta} sectionName={"Women's kurta"}></HomeSectionCarosel>
+                <HomeSectionCarosel data={dressPage1} sectionName={"Women's Dress"}></HomeSectionCarosel>
 
             </div>
         </div>
