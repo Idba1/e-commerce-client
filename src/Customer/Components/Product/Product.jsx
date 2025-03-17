7e4fb20b7c9858acb420b93f79d95e0b0be9626c
Idba1 <moniraislamself@gmail.com>
2025-03-18 03:03:58 +0600

    update new araivel part with mui radio]

--- src/Customer/Components/Product/Product.jsx
+++ src/Customer/Components/Product/Product.jsx
@@ -18,6 +18,7 @@ import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from
 import ProductCard from './ProductCard'
 import { mens_kurta } from '../../../data/men_kurta'
 import { filters, singleFilter } from './FilterData'
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 
 const sortOptions = [
     { name: 'Price: Low to High', href: '#', current: false },
@@ -241,9 +242,12 @@ export default function Product() {
                         </h2>
 
                         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-                            {/* Filters */}
+                            {/*  */}
+                           
                             <form className="hidden lg:block">
-                              
+                            <h1 className='text-lg opacity-50 font-bold'>
+                                Filters
+                            </h1>
 
                                 {filters.map((section) => (
                                     <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
@@ -303,9 +307,11 @@ export default function Product() {
                                 ))}
                                 {singleFilter.map((section) => (
                                     <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
+
                                         <h3 className="-my-3 flow-root">
                                             <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                                                <span className="font-medium text-gray-900">{section.name}</span>
+                                                {/* <span className="font-medium text-gray-900"></span> */}
+                                                <FormLabel sx={{ color: "black" }} className='font-medium text-gray-900' id="demo-radio-buttons-group-label">{section.name}</FormLabel>
                                                 <span className="ml-6 flex items-center">
                                                     <PlusIcon aria-hidden="true" className="size-5 group-data-open:hidden" />
                                                     <MinusIcon aria-hidden="true" className="size-5 group-not-data-open:hidden" />
@@ -314,47 +320,26 @@ export default function Product() {
                                         </h3>
                                         <DisclosurePanel className="pt-6">
                                             <div className="space-y-4">
-                                                {section.options.map((option, optionIdx) => (
-                                                    <div key={option.value} className="flex gap-3">
-                                                        <div className="flex h-5 shrink-0 items-center">
-                                                            <div className="group grid size-4 grid-cols-1">
-                                                                <input
-                                                                    defaultValue={option.value}
-                                                                    defaultChecked={option.checked}
-                                                                    id={`filter-${section.id}-${optionIdx}`}
-                                                                    name={`${section.id}[]`}
-                                                                    type="checkbox"
-                                                                    className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-                                                                />
-                                                                <svg
-                                                                    fill="none"
-                                                                    viewBox="0 0 14 14"
-                                                                    className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
-                                                                >
-                                                                    <path
-                                                                        d="M3 8L6 11L11 3.5"
-                                                                        strokeWidth={2}
-                                                                        strokeLinecap="round"
-                                                                        strokeLinejoin="round"
-                                                                        className="opacity-0 group-has-checked:opacity-100"
-                                                                    />
-                                                                    <path
-                                                                        d="M3 7H11"
-                                                                        strokeWidth={2}
-                                                                        strokeLinecap="round"
-                                                                        strokeLinejoin="round"
-                                                                        className="opacity-0 group-has-indeterminate:opacity-100"
-                                                                    />
-                                                                </svg>
-                                                            </div>
-                                                        </div>
-                                                        <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm text-gray-600">
-                                                            {option.label}
-                                                        </label>
-                                                    </div>
-                                                ))}
+                                                <FormControl>
+                                                    <RadioGroup
+                                                        aria-labelledby="demo-radio-buttons-group-label"
+                                                        defaultValue="option"
+                                                        name="radio-buttons-group"
+                                                    >
+                                                        {section.options.map((option, optionIdx) => (
+
+                                                            <>
+                                                                <FormControlLabel value={option.id} control={<Radio />} label={option.label} />
+                                                            </>
+
+
+                                                        ))}
+                                                    </RadioGroup>
+                                                </FormControl>
                                             </div>
+
                                         </DisclosurePanel>
+
                                     </Disclosure>
                                 ))}
                             </form>
